fe4b1984734d6341d6e3fc8fbbfc4886fa069a63
Mihail <kmihas@mail.ru>
2021-02-02 00:56:06 +0300

    fix error message

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,6 +1,11 @@
 const ERROR_CODES = {
   EMAIL_NOT_FOUND: 'Пользователь с таким email не найден',
   INVALID_PASSWORD: 'Пароль не верный',
+  USER_DISABLED: 'учетная запись пользователя отключена администратором',
+  EMAIL_EXISTS: 'адрес электронной почты уже используется',
+  OPERATION_NOT_ALLOWED: 'пароль для входа в этот проект отключен',
+  TOO_MANY_ATTEMPTS_TRY_LATER: 'мы заблокировали все запросы с этого устройства из-за необычной активности. Попробуйте позже',
+  OPERATION_NOT_ALLOWED: 'вход анонимного пользователя отключен'
 }
 
 export function error(code) {
